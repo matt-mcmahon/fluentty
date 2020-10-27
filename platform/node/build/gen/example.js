9f9049a80888ebabcee8af59b214d47cccd7570f
Matt McMahon <contact@matt-mcmahon.com>
2020-10-27 19:57:29 -0400

    New Build

---
+++ platform/node/build/gen/example.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.example = void 0;
+function example() {
+    return "It works!";
+}
+exports.example = example;
