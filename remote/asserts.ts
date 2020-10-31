7977b1eec012eae80dba46c4e69df312a201ab98
Matt McMahon <contact@matt-mcmahon.com>
2020-10-31 01:26:35 -0400

    remote/asserts.ts: export AssertFunction type

--- remote/asserts.ts
+++ remote/asserts.ts
@@ -1,1 +1,5 @@
-export * from "https://deno.land/std@0.74.0/testing/asserts.ts";
+export * from "https://deno.land/std@0.75.0/testing/asserts.ts";
+
+export interface AssertFunction {
+  (actual: unknown, expected: unknown, msg?: string | undefined): void;
+}
