67059626a753f108e38eaf7167ac1c6e7476204b
Matt McMahon <contact@matt-mcmahon.com>
2020-11-04 18:34:15 -0500

    Upgrade Dependencies

--- dependencies.ts
+++ dependencies.ts
@@ -1,6 +1,6 @@
-import "././remote/asserts.ts";
-import "././remote/path.ts";
-import "././remote/fs.ts";
-import "././remote/colors.ts";
-import "././remote/describe.ts";
-import "././remote/empty_dir.ts";
+import "./remote/asserts.ts";
+import "./remote/path.ts";
+import "./remote/fs.ts";
+import "./remote/colors.ts";
+import "./remote/describe.ts";
+import "./remote/empty_dir.ts";
