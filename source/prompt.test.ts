9c6d9b3b00844b60e731c8ecc7eecb3bc55c0a30
Matt McMahon <contact@matt-mcmahon.com>
2020-11-13 20:06:54 -0500

    prompt.test.ts: use describe

--- source/prompt.test.ts
+++ source/prompt.test.ts
@@ -1,14 +1,15 @@
-import { assertEquals, assertThrowsAsync, fail } from "../remote/asserts.ts";
+import { describe } from "../remote/describe.ts";
+import { assertThrowsAsync, fail } from "../remote/asserts.ts";
 import { brightWhite, dim, stripColor } from "../remote/colors.ts";
 import { Prompt } from "./prompt.ts";
 
-Deno.test("Prompt.prototype.suggestions - sort order", () => {
+describe("Prompt.prototype.suggestions - sort order", ({ assert }) => {
   {
     const a1 = [1, 2, 3];
     const a2 = [4, 2, 5];
     const actual = [...new Set([...a1, ...a2])];
     const expected = [1, 2, 3, 4, 5];
-    assertEquals(actual, expected);
+    assert({ actual, expected });
   }
 
   {
@@ -18,28 +19,28 @@ Deno.test("Prompt.prototype.suggestions - sort order", () => {
     const p3 = p1.concat(p2);
     const actual = stripColor(p3.hint).trim();
     const expected = `(a, b, c, d, e, f)`;
-    assertEquals(actual, expected);
+    assert({ actual, expected });
   }
 });
 
-Deno.test("Prompt.prototype.validate", () => {
+describe("Prompt.prototype.validate", ({ assert }) => {
   const message = "To be or not to be:";
   const prompt = Prompt.from(message);
 
-  assertEquals(prompt.message, message);
+  assert({ actual: prompt.message, expected: message });
 
   {
     // A prompt with zero validators should accept any input
     const actual = prompt.validate("foo");
     const expected = "foo";
-    assertEquals(actual, expected);
+    assert({ actual, expected });
   }
 
   {
     // A prompt with zero validators should accept even empty input
     const actual = prompt.validate("");
     const expected = "";
-    assertEquals(actual, expected);
+    assert({ actual, expected });
   }
 
   const suggestions = ["be", "not be"];
@@ -51,13 +52,13 @@ Deno.test("Prompt.prototype.validate", () => {
   {
     const actual = stripColor(prompt.hint);
     const expected = "(be/not be) ";
-    assertEquals(actual, expected);
+    assert({ actual, expected });
   }
 
   {
     const actual = stripColor(prompt.toString());
     const expected = `To be or not to be: (be/not be) `;
-    assertEquals(actual, expected);
+    assert({ actual, expected });
   }
 
   prompt.defaultTo = "be";
@@ -67,13 +68,13 @@ Deno.test("Prompt.prototype.validate", () => {
     const expected = ["(", "be", "/", "not be", ") "].map(
       (v) => v === "be" ? brightWhite(v) : dim(v),
     ).join("");
-    assertEquals(actual, expected);
+    assert({ actual, expected });
   }
 
   try {
     const actual = prompt.validate("be");
     const expected = "be";
-    assertEquals(actual, expected);
+    assert({ actual, expected });
   } catch (err) {
     fail(err);
   }
@@ -81,7 +82,7 @@ Deno.test("Prompt.prototype.validate", () => {
   try {
     const actual = prompt.validate("not be");
     const expected = "not be";
-    assertEquals(actual, expected);
+    assert({ actual, expected });
   } catch (err) {
     fail(err);
   }
@@ -91,7 +92,7 @@ Deno.test("Prompt.prototype.validate", () => {
   }, TypeError);
 });
 
-Deno.test("Prompt.prototype.concat", () => {
+describe("Prompt.prototype.concat", ({ assert }) => {
   const m1 = "Prompt 1";
   const s1 = (s: string) => s + " world";
   const f1 = (s: string) => `${s.toUpperCase()}!`;
@@ -101,7 +102,8 @@ Deno.test("Prompt.prototype.concat", () => {
     const input = "hello";
     const actual = p1.validate(input);
     const expected = f1(s1(input));
-    assertEquals(actual, expected, p1.toString());
+    const message = p1.toString();
+    assert({ actual, expected, message });
   }
 
   const m2 = "Prompt 2";
@@ -113,7 +115,7 @@ Deno.test("Prompt.prototype.concat", () => {
     const input = "HELLO WORLD!";
     const actual = p2.validate(input);
     const expected = f2(s2(input));
-    assertEquals(actual, expected);
+    assert({ actual, expected });
   }
 
   const p3 = p1.concat(p2);
@@ -122,11 +124,12 @@ Deno.test("Prompt.prototype.concat", () => {
     const input = "hello";
     const actual = p3.validate(input);
     const expected = f2(f1(s2(s1(input))));
-    assertEquals(actual, expected, p1.toString());
+    const message = p1.toString();
+    assert({ actual, expected, message });
   }
 });
 
-Deno.test("Prompt.prototype.defaultTo", () => {
+describe("Prompt.prototype.defaultTo", ({ assert }) => {
   const p1 = Prompt.of({
     message: "I have a default value",
     defaultTo: "default 1",
@@ -135,7 +138,7 @@ Deno.test("Prompt.prototype.defaultTo", () => {
   {
     const actual = p1.validate("");
     const expected = "default 1";
-    assertEquals(actual, expected);
+    assert({ actual, expected });
   }
 
   const p2 = p1.concat({ defaultTo: "default 2" });
@@ -143,7 +146,7 @@ Deno.test("Prompt.prototype.defaultTo", () => {
   {
     const actual = p2.validate("");
     const expected = "default 2";
-    assertEquals(actual, expected);
+    assert({ actual, expected });
   }
 
   const p3 = p2.concat({ validators: [() => false] });
@@ -151,12 +154,12 @@ Deno.test("Prompt.prototype.defaultTo", () => {
   {
     const actual = p3.validate("");
     const expected = "default 2";
-    assertEquals(actual, expected);
+    assert({ actual, expected });
   }
 
   {
     const actual = p3.validate("default 2");
     const expected = "default 2";
-    assertEquals(actual, expected);
+    assert({ actual, expected });
   }
 });
