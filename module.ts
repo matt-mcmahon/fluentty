e2500e2bdb7f0eb24517ed7c1568d1c387e33eff
Matt McMahon <contact@matt-mcmahon.com>
2020-11-13 20:08:50 -0500

    module.ts: add match module to export

--- module.ts
+++ module.ts
@@ -1,4 +1,5 @@
 export * from "./source/io.ts";
+export * from "./source/match.ts";
 export * from "./source/prompt.ts";
 export * from "./source/question.ts";
 export * from "./source/test_process.ts";
